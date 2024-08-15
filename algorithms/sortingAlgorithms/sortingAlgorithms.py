025e8ce444ebdc52e2fb4494ded69a9e7ddb0e34
Mousuf C Zaman <mousuf@live.co.uk>
2024-08-15 09:22:52 +0100

    Added Selection Sort @sorting algorithm

--- algorithms/sortingAlgorithms/sortingAlgorithms.py
+++ algorithms/sortingAlgorithms/sortingAlgorithms.py
@@ -17,3 +17,21 @@ def fisher_yates_shuffle(arr):
         j = random.randint(0, i)
         # Swap the current element with the randomly selected element
         arr[i], arr[j] = arr[j], arr[i]
+
+
+# Selection Sort Algorithm
+class selectionSort:
+    #@staticmethod
+    def selectionSort(self, arr):
+        n = len(arr)
+        
+        for i in range(n - 1):
+            min_index = i
+
+            # Find the index of the minimum element in the unsorted part
+            for j in range(i + 1, n):
+                if arr[j] < arr[min_index]:
+                    min_index = j
+
+            # Swap the minimum element with the first unsorted element
+            arr[i], arr[min_index] = arr[min_index], arr[i]
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from algorithms import algorithmBasics
 from algorithms.sortingAlgorithms import sortingAlgorithms
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+randArr = [13, 4, 16, 8, 14, 5, 18, 11, 15, 2, 17, 12]
 
-#algoClass = algorithmBasics.MaxProductTest()
-#output = algoClass.max_product(arr)
-output = sortingAlgorxithms.fisher_yates_shuffle(arr)
+algoInstance = sortingAlgorithms.selectionSort()
+#output = sortingAlgorithms.fisher_yates_shuffle(arr)
+output = algoInstance.selectionSort(randArr) 
 
 if __name__ == "__main__":
-        print(f"...{arr} ....")
+        print(f"Selection Sort: {randArr} ....")
