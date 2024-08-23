3619d499269c23baa35379edfb47636a9275ad87
Mousuf C Zaman <mousuf@live.co.uk>
2024-08-23 22:58:00 +0100

    Step through debugging

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from algorithms.sortingAlgorithms import sortingAlgorithms
 
 #arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 #randArr = [13, 4, 16, 8, 14, 5, 18, 11, 15, 2, 17, 12]
-randArr = [13, 4, 16, 8, 14  ]
+randArr = [13, 4, 16, 8, 14, 5]
 
 algoInstance = sortingAlgorithms.MergeSort()
 output = algoInstance.mergeSort(randArr, 0, len(randArr) - 1)
