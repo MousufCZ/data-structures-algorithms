7a5480f70f7209b36dd498efd9a03bb739c69548
Mousuf C Zaman <mousuf@live.co.uk>
2024-08-24 10:36:34 +0100

    Linear binary search implemented

--- algorithms/searchAlgorithms/searchAlgorithms.py
+++ algorithms/searchAlgorithms/searchAlgorithms.py
@@ -4,3 +4,29 @@ class SequentialSearch:
         while i < len(arr) and arr[i] != target:
             i = i + 1
         return i
+
+# Binary search
+class BinarySearch:
+    def binarySearchLinear(self, arr, target):
+        # Set the initial search boundaries
+        low = 0
+        high = len(arr) - 1
+
+        # Continue searching while the search space is valid
+        while low <= high:
+            # Calculate the middle index
+            middle = (low + high) // 2
+
+            # Check if the middle element is the target
+            if arr[middle] == target:
+                return middle # Target found, return the index 
+            
+            # If the target is in the right half
+            elif arr[middle] < target:
+                low = middle + 1
+            
+            # If the target is in the left 
+            else:
+                high = middle - 1
+        
+        return -1 # Target not found, return -1
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from algorithms.searchAlgorithms import searchAlgorithms
 
-#arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-randArr = [13, 4, 16, 8, 14, 5, 18, 11, 15, 2, 17, 12]
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#randArr = [13, 4, 16, 8, 14, 5, 18, 11, 15, 2, 17, 12]
 
 algoInstance = searchAlgorithms.SequentialSearch()
-target = 15
-output = algoInstance.sequentialSearch(randArr, target)
+target = 3
+output = algoInstance.sequentialSearch(arr, target)
 
 if __name__ == "__main__":
         print(f"Sequential search: target number {target} is at element {output} in the array ....")
