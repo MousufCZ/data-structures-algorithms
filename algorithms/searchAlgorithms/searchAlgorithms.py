ff4c3d0d4c797b4f8dd743ef65463c7ced28f611
Mousuf C Zaman <mousuf@live.co.uk>
2024-08-25 10:39:57 +0100

    Required to debug recursive binary search

--- algorithms/searchAlgorithms/searchAlgorithms.py
+++ algorithms/searchAlgorithms/searchAlgorithms.py
@@ -21,12 +21,62 @@ class BinarySearch:
             if arr[middle] == target:
                 return middle # Target found, return the index 
             
-            # If the target is in the right half
+            # If the target is in the right half of mid
             elif arr[middle] < target:
                 low = middle + 1
             
-            # If the target is in the left 
+            # If the target is in the left half of mid 
             else:
                 high = middle - 1
         
         return -1 # Target not found, return -1
+    
+
+    def binarySearchRecursive(self, arr, low, high, target):
+        # Set the initial boundaries
+        # low = 0
+        # high = len(arr) - 1
+
+        # Continue searching while the search space is valid
+        while low <= high:
+            # Calculate the middle index
+            middle = (low + high) // 2
+
+
+            # Check if the middle element is the target
+            if arr[middle] == target:
+                return middle
+            
+            # If the target is in the right half of mid
+            elif arr[middle] < target:
+                return BinarySearch.binarySearchRecursive(arr, middle - 1, high, target)
+            
+            # If the target is in the left half of mid 
+            elif arr[middle] > target:
+                return BinarySearch.binarySearchRecursive(arr, low, middle - 1, target)
+            
+        return -1 # Target not found
+    
+    def binarySearch(self, arr, low, high, x):
+        # Check base case
+        if high >= low:
+
+            mid = low + (high - low) // 2
+
+            # If element is present at the middle itself
+            if arr[mid] == x:
+                return mid
+
+            # If element is smaller than mid, then it
+            # can only be present in left subarray
+            elif arr[mid] > x:
+                return binarySearch(self, arr, low, mid-1, x)
+
+            # Else the element can only be present
+            # in right subarray
+            else:
+                return binarySearch(self, arr, mid + 1, high, x)
+
+        # Element is not present in the array
+        else:
+            return -1
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from algorithms.searchAlgorithms import searchAlgorithms
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 #randArr = [13, 4, 16, 8, 14, 5, 18, 11, 15, 2, 17, 12]
 
-algoInstance = searchAlgorithms.SequentialSearch()
+algoInstance = searchAlgorithms.BinarySearch()
 target = 3
-output = algoInstance.sequentialSearch(arr, target)
+
+#b = searchAlgorithms.binarySearch(arr, 0, len(arr) -1, target)
+output = algoInstance.binarySearch(arr, 0, len(arr) -1, target)
 
 if __name__ == "__main__":
-        print(f"Sequential search: target number {target} is at element {output} in the array ....")
+        print(f"Binary search: target number {target} is at element {b} in the array ....")
