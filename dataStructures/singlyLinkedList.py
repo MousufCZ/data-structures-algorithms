d42128f4998c7ad6a6c365648b14ae441784ebd6
Mousuf C Zaman <mousuf@live.co.uk>
2024-09-04 17:41:32 +0100

    Update SLL

--- dataStructures/singlyLinkedList.py
+++ dataStructures/singlyLinkedList.py
@@ -17,3 +17,33 @@ class LinkedList:
         while lastNode.next:
             lastNode = lastNode.next
         lastNode.next = newNode
+
+        def prepend(self, data):
+            newNode = Node(data)
+            newNode.next = self.head
+            self.head = newNode
+
+        def insertAtPosition(self, position, data):
+            if position == 0:
+                self.prepend(data)
+                return
+            
+            newNode = Node(data)
+            currentNode = self.head
+            prevNode = None
+            currentPosition = 0
+
+            while currentNode and currentPosition < position:
+                prevNode = currentNode
+                currentNode = currentNode.next
+                currentPosition += 1
+            prevNode.next = newNode
+            newNode.next = currentNode
+            
+            def deleteNode(self, key):
+                currentNode = self.head
+                if currentNode and currentNode.data == key:
+                    self.head = currentNode.next
+                    currentNode = None
+                    return
+# ============================
