f4da010ffe3ccd9dd8cecffdd836a4fe1437733e
Mousuf C Zaman <mousuf@live.co.uk>
2024-09-05 11:59:05 +0100

    /bin/python3

---
+++ database/randInt.py
@@ -0,0 +1,24 @@
+import random
+"""
+This script will generate random integers in a file
+"""
+# Create a new file
+class FileManupilation:
+    def __init__(self):
+        return
+        #self = self
+
+    def createNewFile(self):
+        #f = open("database/randInt.txt", "w")
+        f = open("database/randInt.txt", "x")
+
+    def generate_rand_no_to_file(self):
+        f = open("database/randInt.txt", "w")
+        for i in range(100):
+            f.write(f"{int(random.randint(1,100))} \n")
+        f.close()
+
+
+if __name__ == "__main__":
+    randInstance = FileManupilation()
+    randInstance.generate_rand_no_to_file()
