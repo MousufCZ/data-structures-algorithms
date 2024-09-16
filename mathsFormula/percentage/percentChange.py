7ab283b10518cd53d9ea3060bf6d638948642c73
Mousuf C Zaman <mousuf@live.co.uk>
2024-09-16 19:51:54 +0100

    Reformatting Maths branch

--- mathsFormula/percentage/percentChange.py
+++ mathsFormula/percentage/percentChange.py
@@ -1,5 +1,21 @@
 import percentOf
 
+"""
+This class takes two @paras to calculate the percentage change of a given 2
+numbers.
+@args 1 = orig_num_input_1
+@args 2 = orig_num_input_2
+
+orig_num_input_1 is the first given number
+orig_num_input_2 is the second given number
+
+Formula
+Step 1: difference between the numbers = orig_num_input_1 - orig_num_input_2
+Step 2: divide the difference = difference between the numbers / orig_num_input_1
+Step 3: the percentage change = divided difference * 100
+
+"""
+
 class PercentChange:
     def __init__(self, orig_num_input_1, orig_num_input_2):
         self.orig_num_input_1 = orig_num_input_1
@@ -36,7 +52,7 @@ class PercentChange:
 
     def class_import(self):
         """Class being imported takes to parameter args.
-        Variables from existing class instantiation. """
+        Variables from existing class instantiation."""
         orig_num_input1 = self.orig_num_input_1
         percent_input = self.orig_num_input_2
 
@@ -47,6 +63,7 @@ class PercentChange:
         classImportTest.total_after_perc_increase()
         classImportTest.total_after_perc_decrease()
 
+"""
 def test_class_import():
     # Hard code parameters
     originalNum1 = 1.2
@@ -60,3 +77,4 @@ def test_class_import():
 
 if __name__ == "__main__":
     test_class_import()
+"""
