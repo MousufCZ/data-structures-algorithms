2c25182052acc655f33dc7df9513b875540d078f
Mousuf C Zaman <mousuf@live.co.uk>
2024-09-24 22:15:10 +0100

    Database ammendments.

--- database/dbImplementationTest/mysql.sql
+++ database/dbImplementationTest/mysql.sql
@@ -2,5 +2,5 @@
 
 -- CREATE TABLE emp(empid INTEGER NOT NULL PRIMARY KEY, empname TEXT NOT NULL, email NOT NULL);
 -- INSERT INTO emp(empid, empname, email)
--- VALUES (2, "Mousuf", "test2@test.com");
+-- VALUES (3, "John", "test3@test.com");
 SELECT * FROM emp;
