d7ba271a96bd9691bedd6b39bfd0b666206ffb2e
Mousuf C Zaman <mousuf@live.co.uk>
2024-09-30 22:04:59 +0100

    Binary tree implementation

---
+++ dataStructures/binaryTree/binartTree.py
@@ -0,0 +1,8 @@
+class TreeNode:
+    def __init__(self, data):
+        """
+        TreeNode constructor to initialize a node with data.
+        """
+        self.data = data
+        self.left = None
+        self.right = None
