172a4da735b6b19d9bae2373387e376e692cb5a2
Mousuf C Zaman <mousuf@live.co.uk>
2024-10-01 22:50:48 +0100

    Insert node data for Binary tree.

--- dataStructures/binaryTree/binartTree.py
+++ dataStructures/binaryTree/binartTree.py
@@ -6,3 +6,17 @@ class TreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if not self.root:
+            # If the tree is empty, set the new node as the root
+            self.root = TreeNode(data)
+        else:
+            # Otherwise, recursively find the appropriate position to insert the new node
+            self.insert_recursive(data, self.root)
+
+    
