f36a239069739061125fff045940032b430f2c7d
Mousuf C Zaman <mousuf@live.co.uk>
2024-10-06 23:38:30 +0100

    Binary tree recursive add.

--- dataStructures/binaryTree/binartTree.py
+++ dataStructures/binaryTree/binartTree.py
@@ -19,3 +19,20 @@ class BinaryTree:
             # Otherwise, recursively find the appropriate position to insert the new node
 #            self.insert_recursive(data, self.root)
             self.insert_recursive(data, self.root)
+
+    def _insert_recursive(self, data, current_node):
+        """
+        Recursive helper function to insert a new node with data at the correct position.
+        """
+        if data < current_node.data:
+            # If data is less than current node, go left
+            if current_node.left is None:
+                current_node.left = TreeNode(data)
+            else:
+                self._insert_recursive(data, current_node.left)
+        elif data > current_node.data:
+            # If data is greater than current node, go right
+            if current_node.right is None:
+                current_node.right = TreeNode(data)
+            else:
+                self._insert_recursive(data, current_node.right)
