a36aba784f7bebbdd048d52ee39b7f33d0e17a6c
Mousuf C Zaman <mousuf@live.co.uk>
2024-10-10 17:28:18 +0100

    Fix nmap.

--- dataStructuresAlgo/main.py
+++ dataStructuresAlgo/main.py
@@ -1,4 +1,4 @@
-import testDSFunctions
+import testDSFunctions as testDSFunctions
 
 if __name__ == "__main__":
         testDSFunction = testDSFunctions.testBinaryTree()
---
+++ network/nmapTest.py
@@ -0,0 +1,40 @@
+import nmap
+
+# Create an instance of the `nmap.PortScanner` class
+nm = nmap.PortScanner()
+
+# Specify the target host or network and the ports to scan
+target = "192.168.1.1"  # Replace with your target
+ports = "1"  # Specify the port range
+
+# Perform the scan
+nm.scan(hosts=target, ports=ports)
+
+def testScan():
+    # Print the scan results
+    print("Scanner info: ", nm.scaninfo())
+    print("Command line: ", nm.command_line())
+    print("All hosts: ", nm.all_hosts())
+    print("Hostname: ", nm[target].hostname())
+    print("State: ", nm[target].state())
+    print("All protocols: ", nm[target].all_protocols())
+    print("Keys: ", nm[target]['tcp'].keys())
+    print("Has TCP port: ", nm[target].has_tcp(1))
+    print("Scanner target information: \n", nm[target])
+
+# https://pypi.org/project/python-nmap/
+def scanPorts():
+    for host in nm.all_hosts():
+        print('----------------------------------------------------')
+        print('Host : %s (%s)' % (host, nm[host].hostname()))
+        print('State : %s' % nm[host].state())
+        for proto in nm[host].all_protocols():
+            print('----------')
+            print('Protocol : %s' % proto)
+            lport = nm[host][proto].keys()
+            lport.sort()
+            for port in lport:
+                print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+
+if __name__ == "__main__":
+    scanPorts()
