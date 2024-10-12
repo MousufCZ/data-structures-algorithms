db98db94f4198a2b7161f115aa2b60f7daadd3db
mousufcz <mousuf.byae@gmail.com>
2024-10-12 21:18:48 +0100

    Implemented a port scanner using nMap

---
+++ documentation/Network/nmap.md
@@ -0,0 +1,6 @@
+# Initial URL
+
+https://pypi.org/project/python-nmap/
+https://nmap.org/book/man.html
+https://www.studytonight.com/network-programming-in-python/integrating-port-scanner-with-nmap
+
--- network/testFunc.py
+++ network/testFunc.py
@@ -4,24 +4,11 @@ import nmap
 nm = nmap.PortScanner()
 
 # Specify the target host or network and the ports to scan
-target = "192.168.1.1"  # Replace with your target
-ports = '1-1000'  # Specify the port range
+target = "192.16.1.1"  # Replace with your target
+ports = '1-200'  # Specify the port range
 
 # Perform the scan
 nm.scan(hosts=target, ports=ports)
-
-def testScan():
-    # Print the scan results
-    print("Scanner info: ", nm.scaninfo())
-    print("Command line: ", nm.command_line())
-    print("All hosts: ", nm.all_hosts())
-    print("Hostname: ", nm[target].hostname())
-    print("State: ", nm[target].state())
-    print("All protocols: ", nm[target].all_protocols())
-    print("Keys: ", nm[target]['tcp'].keys())
-    print("Has TCP port: ", nm[target].has_tcp(1))
-    print("Scanner target information: \n", nm[target])
-
 
 def scanPorts():
     for host in nm.all_hosts():
@@ -38,4 +25,3 @@ def scanPorts():
 
 if __name__ == "__main__":
     scanPorts()
-    #testScan()
