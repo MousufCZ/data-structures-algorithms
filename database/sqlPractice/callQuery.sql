4931ba273dc1e19c4be7d45d80a8ee21ec02a85c
Mousuf C Zaman <mousuf@live.co.uk>
2024-10-19 09:14:06 +0100

    SQL queries.

--- database/sqlPractice/callQuery.sql
+++ database/sqlPractice/callQuery.sql
@@ -1,13 +1,21 @@
 -- Returns table with employees on both tables
+-- SELECT * FROM ProjectStar
+-- INTERSECT
+-- SELECT * FROM ProjectMoon;
+-- 
+-- 
+-- SELECT * FROM ProjectMoon
+-- RIGHT JOIN ProjectStar
+    -- on ProjectMoon.EmpId = ProjectStar.EmpId;
+-- 
+-- 
+-- SELECT * FROM ProjectMoon
+-- WHERE EmpId IN (SELECT EmpId FROM ProjectStar);
+
+-- SELECT * FROM ProjectStar
+    -- EXCEPT
+-- SELECT * FROM ProjectMoon
+
 SELECT * FROM ProjectStar
-INTERSECT
-SELECT * FROM ProjectMoon;
+WHERE EmpId NOT IN (SELECT EmpId FROM ProjectMoon)
 
-
-SELECT * FROM ProjectMoon
-RIGHT JOIN ProjectStar
-    on ProjectMoon.EmpId = ProjectStar.EmpId;
-
-
-SELECT * FROM ProjectMoon
-WHERE EmpId IN (SELECT EmpId FROM ProjectStar);
