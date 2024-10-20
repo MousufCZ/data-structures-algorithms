96c296ac2f28923bd4056884bc72184758db7107
Mousuf C Zaman <mousuf@live.co.uk>
2024-10-20 11:31:04 +0100

    Database structure.

--- database/dbImplementationTest/mysql.sql
+++
@@ -1,6 +0,0 @@
--- https://www.youtube.com/watch?v=JrAiefGNUq8&ab_channel=TutorialBrain
-
--- CREATE TABLE emp(empid INTEGER NOT NULL PRIMARY KEY, empname TEXT NOT NULL, email NOT NULL);
--- INSERT INTO emp(empid, empname, email)
--- VALUES (3, "John", "test3@test.com");
-SELECT * FROM emp;
