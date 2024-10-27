12d2d7a424d1c54a3cb27f7703b2a2f44ab0a103
Mousuf C Zaman <mousuf@live.co.uk>
2024-10-27 20:05:50 +0000

    C tutorial.

--- decTo32BitRep/decTo32Bit.c
+++ decTo32BitRep/decTo32Bit.c
@@ -2,7 +2,7 @@
 #include <string.h>  // Include the header for memcpy
 #include <stdio.h>   // Include the header for printf
 // https://cs.stackexchange.com/questions/21312/problem-in-finding-the-floating-point-representation
-// Day 1: Working on C tutorial on Geek For Geeks
+// Day 1: Working on C tutorial on Geek For Geeks [26th - 27th October]
 uint32_t float_to_binary(float num) {
     uint32_t result;
     memcpy(&result, &num, sizeof(result));  // Copy the raw bytes of the float 'num' into 'result'
